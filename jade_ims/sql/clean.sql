e261e505c040f7137804572036a55253de5e86da
Xuanwo <xuanwo.cn@gmail.com>
2016-06-03 20:43:12 +0800

    remove the sql code

--- jade_ims/sql/clean.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE Supplier;
-DROP TABLE Customer;
-DROP TABLE Product;
-DROP TABLE ImportBill;
-DROP TABLE EnterStock;
-DROP TABLE Sale;
-DROP TABLE LeaveStock;
-DROP TABLE SaleBill;
-DROP TABLE StockPile;
--- jade_ims/sql/schema.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS entries;
-CREATE TABLE entries (
-  id    INTEGER PRIMARY KEY AUTOINCREMENT,
-  title string NOT NULL,
-  text  string NOT NULL
-);
-SELECT *
-FROM entries;
