6d102bdf4c5a45cf77cd94b2f9d0600b12a29c7c
Xuanwo <xuanwo.cn@gmail.com>
2016-06-04 10:22:42 +0800

    add more assets

--- jade_ims/views/home.py
+++ jade_ims/views/home.py
@@ -3,6 +3,11 @@ from flask import Blueprint, render_template
 home = Blueprint('home', __name__)
 
 
+@home.route('/')
+def index():
+    return render_template('index.html')
+
+
 @home.route('/login')
 def login():
     return 'Hello,world!'
