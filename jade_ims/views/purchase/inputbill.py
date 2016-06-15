63a265b9353cfd15afd1efca8e1a8097089444a7
Xuanwo <xuanwo.cn@gmail.com>
2016-06-15 15:22:21 +0800

    finish inputbill

--- jade_ims/views/purchase/inputbill.py
+++ jade_ims/views/purchase/inputbill.py
@@ -1,8 +1,20 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from jade_ims.models import db, InputBill, Supplier, Product
 
 inputbill = Blueprint('inputbill', __name__)
 
 
 @inputbill.route('/purchase/input')
 def list_input():
-    return render_template('purchase/inputbill.html')
+    supplier_data = Supplier.query.all()
+    product_data = Product.query.all()
+    return render_template('purchase/inputbill.html',
+                           product_data=product_data
+                           )
+
+
+@inputbill.route('/purchase/input/add', methods=['POST'])
+def add_input():
+    form = request.form
+    print(form)
+    return redirect(url_for('inputbill.list_input'))
