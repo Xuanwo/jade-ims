be9641ae7cdc202b186c3e6eb268200fdb39fea6
Xuanwo <xuanwo.cn@gmail.com>
2016-06-16 09:39:25 +0800

    finish enterstockbill add

--- jade_ims/views/stock/enterstockbill.py
+++ jade_ims/views/stock/enterstockbill.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-from jade_ims.models import db, InputBill, Product
+from jade_ims.models import db, InputBill, Product, Stock
 
 enterstockbill = Blueprint('enterstockbill', __name__)
 
@@ -23,11 +23,25 @@ def list_enterstock():
                            data=data)
 
 
-@enterstockbill.route('/stock/enter/pass')
+@enterstockbill.route('/stock/enter/pass', methods=['POST'])
 def pass_enterstock():
-    pass
+    form = request.form
+    print(form)
+    inputbill = InputBill.query.get(int(form['inputbillid']))
+    stock = Stock.query.get(int(inputbill.Product_ID))
+    stock.Quantity += inputbill.Quantity
+    if request.method == 'POST':
+        try:
+            db.session.add(stock)
+            db.session.delete(inputbill)
+            db.session.commit()
+            flash('进货单审核成功！', 'success')
+        except:
+            db.session.rollback()
+            flash('进货单审核失败，请重试！', 'danger')
+    return redirect(url_for('stock.list_stock'))
 
 
-@enterstockbill.route('/stock/enter/cancel')
+@enterstockbill.route('/stock/enter/cancel', methods=['POST'])
 def cancel_entersotck():
     pass
